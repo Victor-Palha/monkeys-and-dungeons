dae989b4672cf138246cf211197022a88c678b55
Ash CPC <jv.palha@gmail.com>
2023-07-13 17:50:06 -0300

    Refactore classe page with api

--- src/pages/Classes/ClasseInfo/index.tsx
+++ src/pages/Classes/ClasseInfo/index.tsx
@@ -1,22 +1,48 @@
 import { useEffect, useState } from "react";
 import { ClasseAbilitys, ClasseInfoContainer, ClasseProficiency } from "./styles";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import remarkGfm from "remark-gfm";
+
+interface ClasseInterface {
+    id: string,
+    name: string,
+    class_features: {
+        hit_points: {
+            hit_dice: string,
+            hp_at_1st_level: string,
+            hp_at_higher_levels: string
+        },
+        proficiencies: {
+            armor: string,
+            weapons: string,
+            tools: string,
+            saving_throws: string,
+            skills: string
+        }
+        equipment: string[]
+    },
+    features: Features[]
+}
+
+interface Features {
+    index: string,
+    name: string,
+    content: string
+}
+
 
 interface ClasseInfoProps{
     classe: string
 }
 export function ClasseInfo({classe}: ClasseInfoProps){
-    const [classeInfo, setClasseInfo] = useState<any>({})
-    const [classeProficienciesSkills, setClasseProficienciesSkills] = useState<any>([])
-    const [classeProficienciesTools, setClasseProficienciesTools] = useState<any>([])
+    const [classeInfo, setClasseInfo] = useState<ClasseInterface>({} as ClasseInterface)
     const [loading, setLoading] = useState(true)
 
     async function loadClasseInfo(classe: string){
         setLoading(true)
-        const response = await fetch(`https://www.dnd5eapi.co/api/classes/${classe}`)
+        const response = await fetch(`http://localhost:5000/api/classes?classe=${classe}`)
         const data = await response.json()
-        console.log(data.proficiency_choices[1])
-        setClasseProficienciesSkills(data.proficiency_choices[0])
-        setClasseProficienciesTools(data.proficiency_choices[1])
+
         setClasseInfo(data)
         setLoading(false)
     }
@@ -29,40 +55,35 @@ export function ClasseInfo({classe}: ClasseInfoProps){
             {!loading && (
                 <>
                     <ClasseProficiency>
-                        <h3>{classeInfo.name}</h3>
-                        <h4>Hit Points</h4>
-                        <p><strong>Hit Dice: </strong>1d{classeInfo.hit_die}</p>
-                        <p><strong>Hit Points at 1st Level: </strong>{classeInfo.hit_die} + your Constitution modifier</p>
-                        <p><strong>Hit Points at Higher Levels: </strong>1d{classeInfo.hit_die} + your Constitution modifier per {classeInfo.name} level after 1st</p>
-                        <h4>Proficiencies</h4>
-                        {classeInfo.proficiencies.map((proficiency: any)=>(
-                            <p key={proficiency.index}>{proficiency.name.replace("Saving Throw: ", "")}</p>
-                        ))}
-
-                        {classeProficienciesSkills && classeProficienciesTools && (
-                            <>
-                            {classeProficienciesSkills !== undefined && (
-                                <p><strong>Skills: </strong>Choose {classeProficienciesSkills.choose} from {classeProficienciesSkills.from.options.map((skill: any)=> (
-                                    <span key={skill.item.index}>{skill.item.name.replace("Skill: ", "")}</span>
-                                ))}</p>
-                            )}
-                            {classeProficienciesTools !== undefined && classe === "monk" && (
-                                <p><strong>Tools: </strong>Choose {classeProficienciesTools.choose} from {classeProficienciesTools.from.options.map((tool: any)=> (
-                                    <span key={tool.index}>{tool.choice.desc}</span>
-                                ))}</p>
-                            )}
-                            {classeProficienciesTools !== undefined && classe !== "monk" && (
-                                <p><strong>Tools: </strong>Choose {classeProficienciesTools.choose} from {classeProficienciesTools.from.options.map((tool: any)=> (
-                                    <span key={tool.item.index}>{tool.item.name.replace("Tools: ", "")}</span>
-                                ))}</p>
-                            )}
-                            </>
-                        )
-                        }
-
+                        <h2>{classeInfo.name}</h2>
+                        <h3>Hit Points</h3>
+                        <p><strong>Hit Dice: </strong>{classeInfo.class_features.hit_points.hit_dice}</p>
+                        <p><strong>Hit Points at 1st Level: </strong>{classeInfo.class_features.hit_points.hp_at_1st_level}</p>
+                        <p><strong>Hit Points at Higher Levels: </strong>{classeInfo.class_features.hit_points.hp_at_higher_levels}</p>
+                        <h3>Proficiencies</h3>
+                        <p><strong>Armor: </strong>{classeInfo.class_features.proficiencies.armor}</p>
+                        <p><strong>Weapons: </strong>{classeInfo.class_features.proficiencies.weapons}</p>
+                        <p><strong>Tools: </strong>{classeInfo.class_features.proficiencies.tools}</p>
+                        <p><strong>Saving Throws: </strong>{classeInfo.class_features.proficiencies.saving_throws}</p>
+                        <p><strong>Skills: </strong>{classeInfo.class_features.proficiencies.skills}</p>
+                        <h3>Starting Equipment</h3>
+                        <p>You start with the following items, plus anything provided by your background.</p>
+                        <ul>
+                            {classeInfo.class_features.equipment.map((item) => (
+                                <li key={item}>{item}</li>
+                            ))}
+                        </ul>
                     </ClasseProficiency>
+                
                     <ClasseAbilitys>
-
+                        {classeInfo.features.map((feature)=> (
+                            <div key={feature.index}>
+                                <h2>{feature.name}</h2>
+                                <ReactMarkdown remarkPlugins={[remarkGfm]}>
+                                    {feature.content}
+                                </ReactMarkdown>
+                            </div>
+                        ))}
                     </ClasseAbilitys>
                 </>
             )}
--- src/pages/Classes/ClasseInfo/styles.ts
+++ src/pages/Classes/ClasseInfo/styles.ts
@@ -2,8 +2,38 @@ import { styled } from "styled-components";
 
 export const ClasseInfoContainer = styled.section`
     display: grid;
-    grid-template-columns: 15rem 1fr;
+    grid-template-columns: 20rem 50rem;
+    gap: 20rem;
+    padding: 3rem 10rem;
 `
-export const ClasseProficiency = styled.div``
+export const ClasseProficiency = styled.div`
+    background-image: ${props => props.theme.paperColor};
+    padding: 1rem;
+    border-radius: 6px;
+    height: 41rem;
+h2{
+    
+    border-bottom: 1px solid ${props => props.theme.peperTitleColor};
+}
+h3{
+    margin: 0.5rem;
+} 
+    p{
+        margin-bottom: 0.25rem;
+    }
+    ul{
+        margin-left: 1rem;
+    }
+`
 
-export const ClasseAbilitys = styled.div``
+export const ClasseAbilitys = styled.div`
+    background-image: ${props => props.theme.paperColor};
+    padding: 1rem;
+    h2{
+        border-bottom: 1px solid ${props => props.theme.peperTitleColor};
+        margin: 1rem;
+    }
+    ul{
+        margin-left: 1rem;
+    }    
+`
